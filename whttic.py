a912401df071defef02a6fd727b1fbf12b60c2b0
SIC <bonggutz@gmail.com>
2023-01-21 08:58:56 +0100

    Update whttic.py

--- whttic.py
+++ whttic.py
@@ -1,11 +1,15 @@
 import json
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
-def webhook():
+def handle_webhook():
     data = request.get_json()
-    with open("tiktokdata.json", "w") as outfile:
-        json.dump(data, outfile)
-    return "Data saved to data.json"
+    with open("received_tt_data.json", "w") as json_file:
+        json.dump(data, json_file)
+    return "Data saved to JSON file"
+
+if __name__ == "__main__":
+    app.run(port=os.environ.get("PORT", 3000))
